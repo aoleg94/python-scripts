ab63df2ae5a785ab4b3d7f93b8454aaa8186e774
aoleg94 <aoleg94@users.noreply.github.com>
2016-02-10 18:41:07 +0500

    time-tracker: starting & finishing time direct order, formatting

--- time-tracker.py
+++ time-tracker.py
@@ -14,7 +14,8 @@ def pslist():
     pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
     for pid in pids:
         try:
-            yield open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
+            yield open(os.path.join('/proc', pid, 'cmdline'), 
+                'rb').read()
         except IOError: # proc has already terminated
             continue
 
@@ -30,7 +31,8 @@ def initDB():
     db = S.connect(DBFILE)
     db.executescript('''
         PRAGMA synchronous = FULL;
-        create table if not exists time_tracking(date string, time integer);
+        create table if not exists 
+            time_tracking(date string, time integer);
     ''')
     return db
 
@@ -48,11 +50,11 @@ def mainloop():
         time.sleep(PERIOD)
 
 def hms(dur):
-	h = int(dur/3600.0)
-	dur -= h*3600
-	m = int(dur/60.0)
-	s = int(dur - m*60)
-	return h,m,s
+    h = int(dur/3600.0)
+    dur -= h*3600
+    m = int(dur/60.0)
+    s = int(dur - m*60)
+    return h,m,s
 
 def stat():
     db = initDB()
@@ -72,7 +74,9 @@ def stat():
     finish.laststart=None
     finish.laststop=None
 
-    for date, time in db.execute('''select date, time from time_tracking order by date asc, time asc'''):
+    for date, time in db.execute('''
+        select date, time from time_tracking 
+            order by date asc, time asc'''):
         if finish.lastdate is None:
             finish.lastdate, finish.laststart = date, time
             finish.laststop = time + PERIOD
@@ -91,65 +95,77 @@ def stat():
     finish(None, None)
 
 def stat2():
-	from collections import defaultdict
-	db = initDB()
-	print 'yyyymmdd - hh:mm:ss'
-	print '-------------------'
-	D=defaultdict(list)
-	for date, time in db.execute('''select date, time from time_tracking order by date asc, time asc'''):
-		D[date].append(time)
-	for date in sorted(D):
-		dur = len(D[date])*PERIOD
-		print "%s - %02i:%02i:%02i"%((date,)+hms(dur))
+    from collections import defaultdict
+    db = initDB()
+    print 'yyyymmdd - hh:mm:ss'
+    print '-------------------'
+    D=defaultdict(list)
+    for date, time in db.execute('''
+        select date, time from time_tracking 
+            order by date asc, time asc'''):
+        D[date].append(time)
+    for date in sorted(D):
+        dur = len(D[date])*PERIOD
+        print "%s - %02i:%02i:%02i"%((date,)+hms(dur))
 
 def statm(yearmonth=None,salary_per_hour=SALARY_PER_HOUR_DEFAULT):
-	from collections import defaultdict
-	import time
-	db = initDB()
-	print 'yyyymmdd - hh:mm:ss - salary'
-	print '----------------------------'
-	D=defaultdict(list)
-	salary_per_hour = int(salary_per_hour)
-	if yearmonth is None:
-		tm = time.localtime()
-		y = tm.tm_year
-		m = tm.tm_mon
-		if tm.tm_mday < SALARY_DAY:
-			m -= 1
-			if m == 0:
-				m = 12
-				y -= 1
-		yearmonth = (y, m)
-	elif len(yearmonth) <= 2:
-		tm = time.localtime()
-		yearmonth = (tm.tm_year, int(yearmonth))
-	elif len(yearmonth) <= 6:
-		yearmonth = (int(yearmonth[:4]), int(yearmonth[4:]))
-	daterange = (
-		time.mktime((yearmonth[0], yearmonth[1]+0, SALARY_DAY, 0,0,0, 0,0,-1)),
-		time.mktime((yearmonth[0], yearmonth[1]+1, SALARY_DAY, 0,0,0, 0,0,-1)))
-	for date, time in db.execute('''select date, time from time_tracking where time between ? and ? order by date asc, time asc''', daterange):
-		D[date].append(time)
-	dur_sum = 0
-	for date in sorted(D):
-		dur = len(D[date])*PERIOD
-		dur_sum += dur
-		salary = dur/3600.0 * salary_per_hour
-		print "%s - %02i:%02i:%02i - %6i"%((date,)+hms(dur)+(salary,))
-	print '----------------------------'
-	dur = dur_sum
-	salary = dur/3600.0 * salary_per_hour
-	print "%04i%02i__ - %02i:%02i:%02i - %6i"%(yearmonth+hms(dur)+(salary,))
+    from collections import defaultdict
+    import time
+    db = initDB()
+    print 'yyyymmdd - hh:mm:ss - salary'
+    print '----------------------------'
+    D=defaultdict(list)
+    salary_per_hour = int(salary_per_hour)
+    if yearmonth is None:
+        tm = time.localtime()
+        y = tm.tm_year
+        m = tm.tm_mon
+        if tm.tm_mday < SALARY_DAY:
+            m -= 1
+            if m == 0:
+                m = 12
+                y -= 1
+        yearmonth = (y, m)
+    elif len(yearmonth) <= 2:
+        tm = time.localtime()
+        yearmonth = (tm.tm_year, int(yearmonth))
+    elif len(yearmonth) <= 6:
+        yearmonth = (int(yearmonth[:4]), int(yearmonth[4:]))
+    daterange = (
+        time.mktime(
+            (yearmonth[0], yearmonth[1]+0, SALARY_DAY, 0,0,0, 0,0,-1)),
+        time.mktime(
+            (yearmonth[0], yearmonth[1]+1, SALARY_DAY, 0,0,0, 0,0,-1)))
+    for date, time in db.execute('''
+        select date, time from time_tracking 
+            where time between ? and ? 
+            order by date asc, time asc''', daterange):
+        D[date].append(time)
+    dur_sum = 0
+    for date in sorted(D):
+        dur = len(D[date])*PERIOD
+        dur_sum += dur
+        salary = dur/3600.0 * salary_per_hour
+        print "%s - %02i:%02i:%02i - %6i"%((date,)+hms(dur)+(salary,))
+    print '----------------------------'
+    dur = dur_sum
+    salary = dur/3600.0 * salary_per_hour
+    print "%04i%02i__ - %02i:%02i:%02i - %6i"%(
+        yearmonth+hms(dur)+(salary,))
 
 def schedule(lastN="31"):
-	db = initDB()
-	lastN = int(lastN)
-	print 'yyyymmdd - hh:mm:ss - hh:mm:ss - hh:mm:ss'
-	print '-----------------------------------------'
-	for date, mintime, maxtime, count in db.execute('''select date, min(time), max(time), count(time) from time_tracking group by date order by date desc limit ?''', (lastN,)):
-		mintime = (mintime - time.timezone) % 86400
-		maxtime = (maxtime - time.timezone) % 86400
-		print "%s - %02i:%02i:%02i - %02i:%02i:%02i - %02i:%02i:%02i"%((date,)+hms(mintime)+hms(maxtime)+hms(count*PERIOD))
+    db = initDB()
+    lastN = int(lastN)
+    print 'yyyymmdd - hh:mm:ss - hh:mm:ss - hh:mm:ss'
+    print '-----------------------------------------'
+    for date, mintime, maxtime, count in db.execute('''
+        select * from (select date, min(time), max(time), count(time) 
+            from time_tracking group by date 
+            order by date desc limit ?) order by date asc''', (lastN,)):
+        mintime = (mintime - time.timezone) % 86400
+        maxtime = (maxtime - time.timezone) % 86400
+        print "%s - %02i:%02i:%02i - %02i:%02i:%02i - %02i:%02i:%02i"%(
+            (date,)+hms(mintime)+hms(maxtime)+hms(count*PERIOD))
 
 def main():
     import sys
@@ -160,7 +176,7 @@ def main():
     elif sys.argv[1:2] in (['-m'], ['--month']):
         statm(*sys.argv[2:4])
     elif sys.argv[1:2] in (['-s'], ['--schedule']):
-		schedule(*sys.argv[2:3])
+        schedule(*sys.argv[2:3])
     else:
         stat2()
 
